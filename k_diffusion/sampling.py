49702ee412b4bfdf1931e5f337450d874e382e22
Seunghoon Lee <55037969+hgrsikghrd@users.noreply.github.com>
2023-01-18 02:07:38 +0900

    Why did it get fixed?

--- k_diffusion/sampling.py
+++ k_diffusion/sampling.py
@@ -111,7 +111,8 @@ class BrownianTreeNoiseSampler:
 
     def __call__(self, sigma, sigma_next):
         t0, t1 = self.transform(torch.as_tensor(sigma)), self.transform(torch.as_tensor(sigma_next))
-        return self.tree(t0, t1) / (t1 - t0).abs().sqrt()
+        tree = self.tree(t0, t1)
+        return tree / (t1 - t0).view(1, 1, 1, 1).abs().sqrt().repeat(tree.shape)
 
 
 @torch.no_grad()
