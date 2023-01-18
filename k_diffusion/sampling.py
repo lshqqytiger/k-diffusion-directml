d58b265d2c7c8b97b448fb275185e414e2c967b7
Seunghoon Lee <lshqqytiger@naver.com>
2023-01-18 18:22:39 +0900

    Add DirectML support to DPM fast/adaptive, PLMS.

--- k_diffusion/sampling.py
+++ k_diffusion/sampling.py
@@ -2,12 +2,16 @@ import math
 
 from scipy import integrate
 import torch
+import torch_directml
 from torch import nn
 from torchdiffeq import odeint
 import torchsde
 from tqdm.auto import trange, tqdm
 
 from . import utils
+
+
+dml = torch_directml.device(torch_directml.default_device())
 
 
 def append_zero(x):
@@ -433,8 +437,8 @@ class DPMSolver(nn.Module):
         if not forward and eta:
             raise ValueError('eta must be 0 for reverse sampling')
         h_init = abs(h_init) * (1 if forward else -1)
-        atol = torch.tensor(atol)
-        rtol = torch.tensor(rtol)
+        atol = torch.tensor(atol).to(dml)
+        rtol = torch.tensor(rtol).to(dml)
         s = t_start
         x_prev = x
         accept = True
@@ -488,7 +492,7 @@ def sample_dpm_fast(model, x, sigma_min, sigma_max, n, extra_args=None, callback
         dpm_solver = DPMSolver(model, extra_args, eps_callback=pbar.update)
         if callback is not None:
             dpm_solver.info_callback = lambda info: callback({'sigma': dpm_solver.sigma(info['t']), 'sigma_hat': dpm_solver.sigma(info['t_up']), **info})
-        return dpm_solver.dpm_solver_fast(x, dpm_solver.t(torch.tensor(sigma_max)), dpm_solver.t(torch.tensor(sigma_min)), n, eta, s_noise, noise_sampler)
+        return dpm_solver.dpm_solver_fast(x, dpm_solver.t(torch.tensor(sigma_max).to(dml)), dpm_solver.t(torch.tensor(sigma_min).to(dml)), n, eta, s_noise, noise_sampler)
 
 
 @torch.no_grad()
@@ -500,7 +504,7 @@ def sample_dpm_adaptive(model, x, sigma_min, sigma_max, extra_args=None, callbac
         dpm_solver = DPMSolver(model, extra_args, eps_callback=pbar.update)
         if callback is not None:
             dpm_solver.info_callback = lambda info: callback({'sigma': dpm_solver.sigma(info['t']), 'sigma_hat': dpm_solver.sigma(info['t_up']), **info})
-        x, info = dpm_solver.dpm_solver_adaptive(x, dpm_solver.t(torch.tensor(sigma_max)), dpm_solver.t(torch.tensor(sigma_min)), order, rtol, atol, h_init, pcoeff, icoeff, dcoeff, accept_safety, eta, s_noise, noise_sampler)
+        x, info = dpm_solver.dpm_solver_adaptive(x, dpm_solver.t(torch.tensor(sigma_max).to(dml)), dpm_solver.t(torch.tensor(sigma_min).to(dml)), order, rtol, atol, h_init, pcoeff, icoeff, dcoeff, accept_safety, eta, s_noise, noise_sampler)
     if return_info:
         return x, info
     return x
